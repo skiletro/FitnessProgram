b8a9cb6a7071b64759b91d3d8e63a31fa4e595ac
skiletro <19377854+skiletro@users.noreply.github.com>
2024-05-02 23:28:33 +0100

    add onboarding feature

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,34 @@
+import { ZeroedDate, getSelectValues } from "./libraries/handyFunctions.js";
 import "./libraries/imageMapResizer.min.js";
 import { createCalendarElements, updateText, updateCalendarElements } from "./modules/calendarLogic.js";
 import "./modules/diagramSelector.js";
 import "./modules/exerciseViewer.js";
 import "./modules/settingsLogic.js";
+import { setAccentColor } from "./modules/settingsLogic.js";
 
-var selectedDate = new Date();
-selectedDate.setHours(0,0,0,0);
+var selectedDate = ZeroedDate();
 
 document.addEventListener("DOMContentLoaded", () => {
+    // Create the calendar (without this, the calendar won't be loaded at all)
     createCalendarElements(selectedDate);
+    // Update the text in relation to the calendar and the current day
     updateText(selectedDate);
+
+    // Set the preference color
+    if ("preferenceAccentColor" in localStorage) {
+        let accentColor = localStorage.getItem("preferenceAccentColor");
+        document.querySelector(":root").style.setProperty("--accent", accentColor)
+    }
+
+    // If the user needs to be onboarded, then open the onboarding dialog
+    if (("preferenceOnboarding" in localStorage) == false) {
+        document.getElementById("onboarding").open = true;
+    }
 });
 
 // Navigation Buttons
 document.getElementById("navExercises").addEventListener("click", () => {
     document.getElementById("exerciseViewer").open = true;
-});
-
-document.getElementById("navAbout").addEventListener("click", () => {
-    document.getElementById("aboutDialog").open = true;
 });
 
 function openDiagramPicker() {
@@ -30,6 +40,26 @@ document.getElementById("addButton").addEventListener("click", openDiagramPicker
 
 document.getElementById("navSettings").addEventListener("click", () => {
     document.getElementById("settingsDialog").open = true;
+});
+
+// Onboarding
+let onboardingColorPicker = document.getElementById("accentOption");
+onboardingColorPicker.addEventListener("input", () => {
+    setAccentColor(onboardingColorPicker);
+});
+
+document.getElementById("submitOnboarding").addEventListener("click", () => {
+    let difficultyOption = document.getElementById("difficultyOption");
+    let equipmentOption = document.getElementById("equipmentOption");
+
+    if (getSelectValues(equipmentOption).length > 0) {
+        localStorage.setItem("preferenceOnboarding", true);
+        localStorage.setItem("preferenceDifficulty", difficultyOption.value);
+        localStorage.setItem("preferenceEquipment", getSelectValues(equipmentOption).join("|"));
+        document.getElementById("onboarding").open = false;
+    } else {
+        alert("Please make sure you select at least one equipment option.");
+    }
 });
 
 // Calendar
@@ -47,8 +77,7 @@ document.getElementById("gotoPreviousMonth").addEventListener("click", () => {
     updateCalendarElements(selectedDate);
 });
 document.getElementById("gotoToday").addEventListener("click", () => {
-    selectedDate = new Date();
-    selectedDate.setHours(0,0,0,0);
+    selectedDate = ZeroedDate();
     updateCalendarElements(selectedDate);
 });
 document.getElementById("gotoNextMonth").addEventListener("click", () => {
